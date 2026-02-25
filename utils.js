5fa3e2c4ca81c9f1e35eab5474a0e3fcc0727cae
tahir_akdg <124049984+tahirakdag@users.noreply.github.com>
2026-02-25 21:47:43 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-25T18:47:16.191Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-02-25T18:47:41.628Z
+Commit: Fix CSS styling bug on mobile devices
